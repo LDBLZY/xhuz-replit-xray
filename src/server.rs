8ea8738302dbba3514db054595f9fe8409f8cee0
LDBLZY <91302495+LDBLZY@users.noreply.github.com>
2023-02-27 16:06:35 +0800

    Update server.rs

--- src/server.rs
+++ src/server.rs
@@ -162,7 +162,7 @@ impl Server {
             Ok(u) => u,
             Err(_) => {
                 /*let u = Uuid::new_v4();*/
-                let u = "qwertyuiop";
+                let u = "02b12ef2-8aed-48c9-925a-beb5bc482e07";
                 self.set_uuid_to_replit_db(&u)
                     .expect("write uuid to db failed");
                 u.to_string()
